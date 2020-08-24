29d5fe5717fe29a51f2ffea8acc8cefe62760b07
dkgian <duykhanhgian0510@gmail.com>
2020-08-24 11:29:26 +0200

    added contents for ChatItem

--- src/components/ChatItem/ChatItem.js
+++ src/components/ChatItem/ChatItem.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
+import { Avatar } from '@material-ui/core'
+
+import './ChatItem.css'
+
 export const ChatItem = () => {
-  return <div className="chatItem">Chat item</div>
+  return (
+    <div className="chatItem">
+      <div className="chatItem__avatar">
+        <Avatar />
+      </div>
+      <div className="chatItem__content">
+        <div className="chatItem__room">Room 9</div>
+        <div className="chatItem__last">qweqweqwe</div>
+      </div>
+    </div>
+  )
 }
