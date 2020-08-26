a8448136060a3ddb06811521216381838f0c35f4
dkgian <duykhanhgian0510@gmail.com>
2020-08-26 17:32:41 +0200

    fixed styling chatroom list

--- src/components/ChatItem/ChatItem.js
+++ src/components/ChatItem/ChatItem.js
@@ -7,16 +7,16 @@ import { Link } from 'react-router-dom'
 
 export const ChatItem = ({ room, id }) => {
   return (
-    <Link to={`/${id}`}>
-      <div className="chatItem">
-        <div className="chatItem__avatar">
+    <div className="chatItem">
+      <Link to={`/${id}`}>
+        {/* <div className="chatItem__avatar"> */}
           <Avatar />
-        </div>
+        {/* </div> */}
         <div className="chatItem__content">
           <div className="chatItem__room">{room.name}</div>
           <div className="chatItem__last">{id}</div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   )
 }
