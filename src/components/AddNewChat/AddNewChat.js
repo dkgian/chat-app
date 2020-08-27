08e2b447468f7fd337815d19172f836125a5727b
dkgian <duykhanhgian0510@gmail.com>
2020-08-27 15:21:55 +0200

    added addNewChat fn

--- src/components/AddNewChat/AddNewChat.js
+++ src/components/AddNewChat/AddNewChat.js
@@ -2,13 +2,25 @@ import React from 'react'
 
 import Button from '@material-ui/core/Button'
 import AddIcon from '@material-ui/icons/Add'
-
+import db from '../../firebase'
 import './AddNewChat.css'
+
+const addNewChat = () => {
+  return () => {
+    const newRoom = prompt('Add a new chat')
+    if (newRoom) {
+      db.collection('rooms').add({
+        name: newRoom
+      })
+    }
+    return
+  }
+}
 
 export const AddNewChat = () => {
   return (
     <div className="addNewChat">
-      <Button onClick={() => prompt('Add a new chat')}>
+      <Button onClick={addNewChat()}>
         <AddIcon />
         Add a new chat
       </Button>
